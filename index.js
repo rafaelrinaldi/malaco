5a1a8eb8ddcc95ae82c078d28fadfc4cd41f5fad
Rafael Rinaldi <rafael.rinaldi@gmail.com>
2014-04-27 23:30:08 -0300

    Added get address service.

--- index.js
+++ index.js
@@ -5,31 +5,15 @@ var getLines = require('./lib/services/getLines'),
     getStatus = require('./lib/services/getStatus'),
     getGeocode = require('./lib/services/getGeocode');
 
-// getLines()
-//   .then(function(response) {
-//     traceLines(response)
-//   })
+getLines()
 
-function traceLines(model) {
-  // var stations;
-  // Object.keys(model).forEach(function(line) {
-    // stations = model[line];
-    // stations.forEach(function(station) {
-      // console.log(line, '->',station);
-      // getGeocode()
-    // })
-  // });
-}
-// line: 'linha-3-vermelha',
-//   station: 'estacao-barra-funda'
-getStation({
-  line: 'linha-1-azul',
-  station: 'estacao-sao-bento'
-})
-.then(function(response) {
-  console.log(response);
-});
-
+// getStation({
+//   line: 'linha-1-azul',
+//   station: 'estacao-sao-bento'
+// })
+// .then(function(response) {
+//   console.log(response);
+// });
 
 // getStatus()
 //   .then(function(response) {
